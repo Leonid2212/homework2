4cd5a3da72ec5ccdc090f3216db6475cfc80e226
Leonid <Lstrelcov276@gmail.com>
2025-08-01 19:23:24 +0300

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static Object cat;
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog );
+        System.out.println(dog = dog +4);
+        System.out.println(dog = dog - 3.5);
+
+        System.out.println(cat );
+        System.out.println(cat = cat +4);
+        System.out.println(cat = cat - 1.6);
+
+        System.out.println(paper );
+        System.out.println(paper = paper +4);
+        System.out.println(paper= paper - 7639);
+
+        var friend = 19;
+
+        System.out.println(friend  );
+        System.out.println(friend = friend + 2);
+        System.out.println(friend = friend / 7);
+
+        var frog = 3.5;
+        System.out.println(frog );
+        System.out.println(frog = frog + 10);
+        System.out.println(frog = frog / 3.5);
+        System.out.println(frog = frog / 3.5 + 4);
+
+        var b1 =78.2;
+        var b2 = 82.7;
+        System.out.println(b1 + b2);
+        System.out.println(b2 - b1);
+        System.out.println(b2 % b1);
+        var time = 640;
+        System.out.println(640/8);
+        System.out.println(640/8 + 94);
+        System.out.println(174*8);
+
+
+
+
+
     }
-}
+    }
